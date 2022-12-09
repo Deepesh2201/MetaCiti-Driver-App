e6a406e7f9f91b679740c6e715abb79cd59ac66f
JosephDedicated <infotostalin@gmail.com>
2022-12-09 17:18:24 +0530

    data type changed

--- lib/pages/login/login.dart
+++ lib/pages/login/login.dart
@@ -423,7 +423,7 @@ class _LoginState extends State<Login> {
                           height: media.height * 0.1,
                         ),
                         (controller.text.length >=
-                                    int.parse(countries[phcode]['dial_min_length']) &&
+                                    countries[phcode]['dial_min_length'] &&
                                 terms == true)
                             ? Container(
                                 width: media.width * 1 - media.width * 0.08,
